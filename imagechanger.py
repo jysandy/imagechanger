04bb11202fae08b64f85aad2049ee9e12e45d110
Sandilya Jandhyala <jysandilya@gmail.com>
2014-12-31 23:58:41 +0530

    Now you can actually do useful conversions

--- imagechanger.py
+++ imagechanger.py
@@ -5,7 +5,7 @@ from werkzeug import secure_filename
 from PIL import Image
 
 ON_OPENSHIFT = os.environ.get('OPENSHIFT_REPO_DIR', False)
-ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'gif', 'png'])
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'gif', 'png', 'bmp'])
 
 app = Flask(__name__, static_folder = 'wsgi/static')
 app.config['APP_PATH'] = os.path.dirname(os.path.abspath(__file__))
@@ -21,28 +21,36 @@ else:
 @app.route('/', methods = ['GET', 'POST'])
 def main_page():
 	if request.method == 'POST':
-		filename = os.path.join(app.config['TEMP_DIR'], secure_filename(request.files['file'].filename))
-		request.files['file'].save(filename)
-		newname, e = os.path.splitext(filename)
-		newname = newname + '.' + request.form['format']
-		if newname != filename:
-			try:
-				Image.open(filename).save(newname)
-			except IOError as e:
-				print 'Could not convert'
-				print e.strerror
-				return '<h1>Error converting</h1>'
-		@flask.after_this_request
-		def cleanup_files(response):
-			os.remove(filename)
-			os.remove(newname)
-			return response
-		return send_file(newname, as_attachment = True)
+		file = request.files.get('file', False)
+		if file and has_allowed_extension(file.filename):
+			filename = os.path.join(app.config['TEMP_DIR'], secure_filename(file.filename))
+			request.files['file'].save(filename)
+			newname, e = os.path.splitext(filename)
+			newname = newname + '.' + request.form['format']
+			if newname != filename:
+				try:
+					Image.open(filename).save(newname)
+				except IOError as e:
+					print 'Could not convert'
+					print e.strerror
+					return '<h1>Error converting</h1>'
+			@flask.after_this_request
+			def cleanup_files(response):
+				#Remove files if they exist
+				try:
+					os.remove(filename)
+					os.remove(newname)
+				except OSError:
+					pass
+				return response
+			return send_file(newname, as_attachment = True)
 	
 	return render_template('main_page.html')
 
 
-
+def has_allowed_extension(filename):
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 if __name__ == '__main__':
